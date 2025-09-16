cda68a7a859c6d1e3804d7cd27aa60d463690dce
Ahmed Bodiwala <ahmed_bodi@msn.com>
2025-09-17 00:37:18 +0100

    feat: Meowcoin

---
+++ apps/meowcoin/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for telestaid"
+
+  set -- telestaid "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "telestaid" ]; then
+  mkdir -p "$TELESTAI_DATA"
+  chmod 700 "$TELESTAI_DATA"
+  chown -R catcoin "$TELESTAI_DATA"
+
+  echo "$0: setting data directory to $TELESTAI_DATA"
+
+  set -- "$@" -datadir="$TELESTAI_DATA"
+fi
+
+if [ "$1" = "telestaid" ] || [ "$1" = "telestai-cli" ]; then
+  echo
+  exec su-exec telestai "$@"
+fi
+
+echo
+exec "$@"
